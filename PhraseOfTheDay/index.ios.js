39474b3fff0c75d16aacd5831536e4d8124bf8fa
josianeReis <josiane-reis@outlook.com>
2017-10-29 20:24:25 -0200

    creating a simple phrases App

---
+++ PhraseOfTheDay/index.ios.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+AppRegistry.registerComponent('PhraseOfTheDay', () => App)
