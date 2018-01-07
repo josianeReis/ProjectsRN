6040645ec70dd84046c58dfad6041162d7042e3c
Algar Telecom <josiane-reis@outlook.com>
2018-01-07 14:12:18 -0200

    creation of the jokenpo method

--- JokenpoGame/index.ios.js
+++ JokenpoGame/index.ios.js
@@ -6,39 +6,91 @@ import {
   Button
 } from 'react-native';
 
-class MyComponent extends Component {
-  render() {
-    return (
-      <View>
-        <Text>{this.props.teste}</Text>
-      </View>
-    );
-  }
-}
-
 class JokenpoGame extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      texto: 'texto teste 2'
+      escolhaUsuario: '',
+      escolhaComputador: '',
+      resultadoFinal: '',
     };
   }
 
-  changeText() {
-    this.setState({ texto: 'texto mudado com setState' });
+  jokenpo(escolha) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let escolhaMaquina = '';
+
+    switch (randomNumber) {
+      case 0: escolhaMaquina = 'Pedra';
+            break;
+      case 1: escolhaMaquina = 'Papel';
+            break;
+      case 2: escolhaMaquina = 'Tesoura';
+            break;
+      default:
+            break;
+    }
+
+    var resultado = '';
+
+    if (escolhaMaquina === 'Pedra') {
+      if (escolha === 'Pedra') {
+        resultado = 'Empate';
+      }
+      if (escolha === 'Papel') {
+        resultado = 'Usuário Ganhou';
+      }
+      if (escolha === 'Tesoura') {
+        resultado = 'Computador Ganhou';
+      }
+    } else if (escolhaMaquina === 'Papel') {
+      if (escolha === 'Papel') {
+        resultado = 'Empate';
+      }
+      if (escolha === 'Pedra') {
+        resultado = 'Computador Ganhou';
+      }
+      if (escolha === 'Tesoura') {
+        resultado = 'Usuário Ganhou';
+      }
+    } else if (escolhaMaquina === 'Tesoura') {
+      if (escolha === 'Tesoura') {
+        resultado = 'Empate';
+      }
+      if (escolha === 'Pedra') {
+        resultado = 'Usuário Ganhou';
+      }
+      if (escolha === 'Papel') {
+        resultado = 'Computador Ganhou';
+      }
+    }
+
+    this.setState({
+      escolhaUsuario: escolha,
+      escolhaComputador: escolhaMaquina,
+      resultadoFinal: resultado,
+    });
   }
 
   render() {
     return (
       <View>
-        <MyComponent
-          teste={this.state.texto}
+        <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
+        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
+        <Text>Resultado: {this.state.resultadoFinal}</Text>
+        <Button
+          title="Pedra"
+          onPress={() => { this.jokenpo('Pedra'); }}
         />
         <Button
-          title="Botao teste"
-          onPress={() => { this.changeText(); }}
+          title="Papel"
+          onPress={() => { this.jokenpo('Papel'); }}
+        />
+        <Button
+          title="Tesoura"
+          onPress={() => { this.jokenpo('Tesoura'); }}
         />
       </View>
     );
