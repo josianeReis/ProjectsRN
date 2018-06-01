56e6d601ed68253ce7e89e8c7509ab73db81a559
josianereis <josiane-reis@outlook.com>
2018-06-01 16:50:56 -0300

    init movie app

---
+++ theMovieApp/src/ui/icons/svgs/menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Path, G } from 'react-native-svg'
+
+// viewBox="0 0 53 53"
+
+const svg = (
+  <G>
+    <Path d="M2 13.5h49a2 2 0 0 0 0-4H2a2 2 0 0 0 0 4zm0 15h49a2 2 0 0 0 0-4H2a2 2 0 0 0 0 4zm0 15h49a2 2 0 0 0 0-4H2a2 2 0 0 0 0 4z" />
+  </G>
+)
+
+export default svg
